8db405e6787b7ab24a95069e8a806ebffa6d0507
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-04 09:18:38 +0600

    deference in var let and const

---
+++ var-let-const.js
@@ -0,0 +1,27 @@
+// var let const
+
+
+// var can changed the value in same variable
+// but var not required to use
+var team1 = 1000;
+team1 = 2000;
+console.log(team1);
+// ans :- 2000
+
+// let is a es6 version.let also can change the value in same variable
+
+// required to use
+let team2 = 5000;
+team2 = 7000;
+console.log(team2)
+// ans :- 7000
+
+// const is  es6 version .const can't change the value in same variable 
+
+// required to use
+const team3 = 8000; 
+console.log(team3)
+// ans :- 8000
+team3 = 9000;
+console.log(team3)
+
